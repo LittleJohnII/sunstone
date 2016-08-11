223b308ced0382ebdc5fbe5ff03c4efff72e1cf1
Josef Cacek <josef.cacek@gmail.com>
2016-08-11 20:30:14 +0200

    node leak test improved

--- core/src/test/java/org/wildfly/extras/sunstone/api/impl/docker/NodeLeakOnBadPortsTest.java
+++ core/src/test/java/org/wildfly/extras/sunstone/api/impl/docker/NodeLeakOnBadPortsTest.java
@@ -1,40 +1,33 @@
 package org.wildfly.extras.sunstone.api.impl.docker;
 
+import static org.junit.Assert.assertTrue;
+
 import org.jclouds.compute.ComputeService;
-import org.jclouds.compute.domain.ComputeMetadata;
+import org.jclouds.compute.domain.NodeMetadata;
 import org.junit.Assert;
 import org.junit.Test;
 import org.wildfly.extras.sunstone.api.CloudProperties;
 import org.wildfly.extras.sunstone.api.CloudProvider;
-import org.wildfly.extras.sunstone.api.impl.AbstractJCloudsCloudProvider;
+import org.wildfly.extras.sunstone.api.PortOpeningException;
 
-import java.util.Optional;
-
+/**
+ * Checks if node is correctly closed when the expected "start-up port" is not opened in given timeout.
+ */
 public class NodeLeakOnBadPortsTest {
 
     @Test
     public void test() {
-
         CloudProperties.getInstance().reset().load(this.getClass());
-        CloudProvider cloudProvider = CloudProvider.create("provider0");
-
-        try {
-            cloudProvider.createNode("node0");
-            Assert.fail("Port 2468 was unexpectedly opened");
-        } catch (Exception e) {
-            if (cloudProvider instanceof AbstractJCloudsCloudProvider) {
-                ComputeService computeService = ((AbstractJCloudsCloudProvider) cloudProvider).getComputeServiceContext().getComputeService();
-                Optional<? extends ComputeMetadata> latestNode = computeService.listNodes().stream().findFirst();
-                if (latestNode.isPresent()) {
-                    if (latestNode.get().toString().contains("status=RUNNING")) {
-                        computeService.destroyNode(latestNode.get().getId());
-                        Assert.fail("A node was leaked, and it had to be forcefully cleaned");
-                    }
-                }
-            } else {
-                throw new RuntimeException("Cloud provider " + cloudProvider.getName()
-                        + " is not an implementation of AbstractJCloudsCloudProvider and does not support the methods needed for this test. "
-                        + "Go clean up the docker image, it may still be running.");
+        try (DockerCloudProvider cloudProvider = (DockerCloudProvider) CloudProvider.create("provider0")) {
+            final ComputeService computeService = cloudProvider.getComputeServiceContext().getComputeService();
+            assertTrue("No node-group should be 'node-leak-test' before test", computeService.listNodes().stream()
+                    .noneMatch(cm -> "node-leak-test".equals(((NodeMetadata) cm).getGroup())));
+            try {
+                cloudProvider.createNode("node0").close();
+                Assert.fail("Port 2468 was unexpectedly opened");
+            } catch (PortOpeningException e) {
+                assertTrue("No node-group should be 'node-leak-test' after test", computeService.listNodes().stream()
+                        .noneMatch(cm -> "node-leak-test".equals(((NodeMetadata) cm).getGroup())));
             }
         }
     }
